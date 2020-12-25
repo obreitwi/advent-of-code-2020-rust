882586646278292059652e20ae7dc711da702dec
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2020-12-25 18:21:01 +0100

    Day 14 part 2

--- day_14/src/main.rs
+++ day_14/src/main.rs
@@ -25,25 +25,31 @@ fn main() -> Result<()> {
     // println!("Read: {:#?}", program);
 
     part1(&program);
+    part2(&program);
 
     Ok(())
 }
+
+const BITWIDTH: usize = 36;
 
 #[derive(Clone, Debug)]
 struct Bitmask {
     // inverted bitmask to filter out all bits set to specific value
     bitmask_0_inv: u64,
     bitmask_1: u64,
+
+    floating: Vec<usize>, // floating bits, LSB is 0, MSB is BITWIDTH
 }
 
 impl Bitmask {
     pub fn empty() -> Bitmask {
-        let bitmask_0_inv = (1 << 36) - 1;
+        let bitmask_0_inv = (1 << BITWIDTH) - 1;
         let bitmask_1 = 0;
 
         Self {
             bitmask_1,
             bitmask_0_inv,
+            floating: Vec::new(),
         }
     }
 
@@ -57,17 +63,18 @@ impl Bitmask {
 
         let mut bitmask_0_inv = 0;
         let mut bitmask_1 = 0;
+        let mut floating = Vec::new();
 
-        for m in mask.iter() {
+        for (i, m) in mask.iter().enumerate() {
             bitmask_0_inv = bitmask_0_inv << 1;
             bitmask_1 = bitmask_1 << 1;
 
             match m {
                 'X' => {
                     bitmask_0_inv += 1;
+                    floating.push(BITWIDTH - 1 - i);
                 }
-                '0' => {
-                }
+                '0' => {}
                 '1' => {
                     bitmask_0_inv += 1;
                     bitmask_1 += 1;
@@ -77,26 +84,52 @@ impl Bitmask {
                 }
             };
         }
+        floating.sort();
         Ok((
             i,
             Self {
                 bitmask_1,
                 bitmask_0_inv,
+                floating,
             },
         ))
+    }
+
+    pub fn generate_addresses(&self, base: usize) -> Vec<usize> {
+        let mut rv = Vec::new();
+
+        let base = base | self.bitmask_1 as usize;
+
+        for count in 0..(1 << self.floating.len()) {
+            let mut current = base;
+            for (count_idx, float_idx) in self.floating.iter().enumerate() {
+                // delete bit at float_idx in current
+                if current & (1 << float_idx) > 0 {
+                    current -= 1 << float_idx;
+                }
+
+                // write bit at count_idx from count to float_idx in current
+                if count & (1 << count_idx) > 0 {
+                    current += 1 << float_idx;
+                }
+            }
+            rv.push(current);
+        }
+
+        rv
     }
 }
 
 #[derive(Debug)]
 struct Assignment {
-    idx: usize,
+    address: usize,
     value: u64,
 }
 
 impl Assignment {
     pub fn parse(i: &str) -> IResult<&str, Self> {
         let (i, _) = tag("mem[")(i)?;
-        let (i, idx) = digit1(i)?;
+        let (i, address) = digit1(i)?;
         let (i, _) = char(']')(i)?;
         let (i, _) = space0(i)?;
         let (i, _) = char('=')(i)?;
@@ -106,7 +139,7 @@ impl Assignment {
         Ok((
             i,
             Self {
-                idx: idx.parse::<usize>().unwrap(),
+                address: address.parse::<usize>().unwrap(),
                 value: value.parse::<u64>().unwrap(),
             },
         ))
@@ -171,7 +204,30 @@ impl Program {
             match instr {
                 Mem(assign) => {
                     let value = assign.value & mask.bitmask_0_inv | mask.bitmask_1;
-                    memory.insert(assign.idx, value);
+                    memory.insert(assign.address, value);
+                }
+                Mask(m) => {
+                    mask = m.clone();
+                }
+            }
+        }
+
+        memory
+    }
+
+    pub fn run_v2(&self) -> HashMap<usize, u64> {
+        use Instruction::*;
+        let mut memory = HashMap::new();
+
+        let mut mask = Bitmask::empty();
+
+        for instr in self.instructions.iter() {
+            match instr {
+                Mem(assign) => {
+                    for address in mask.generate_addresses(assign.address)
+                    {
+                        memory.insert(address, assign.value);
+                    }
                 }
                 Mask(m) => {
                     mask = m.clone();
@@ -188,4 +244,11 @@ fn part1(prog: &Program) {
     let sum: u64 = mem.values().sum();
 
     println!("(part1) Sum of all elements: {}", sum);
+}
+
+fn part2(prog: &Program) {
+    let mem = prog.run_v2();
+    let sum: u64 = mem.values().sum();
+
+    println!("(part2) Sum of all elements: {}", sum);
 }
