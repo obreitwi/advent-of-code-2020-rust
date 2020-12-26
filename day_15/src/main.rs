c289769609f88938e94c7f3ce23501de7dd92aab
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2020-12-26 01:32:57 +0100

    Day 15 part 2
    original solution fast enough with --release

--- day_15/src/main.rs
+++ day_15/src/main.rs
@@ -1,8 +1,9 @@
-use std::collections::{HashMap, VecDeque};
+use std::collections::{BTreeMap, HashMap, VecDeque};
 
 fn main() {
     let input: Vec<usize> = vec![8, 13, 1, 0, 18, 9];
 
+    assert_eq!(play(&[0, 3, 6], 2020), 436);
     assert_eq!(play(&[1, 3, 2], 2020), 1);
     assert_eq!(play(&[2, 1, 3], 2020), 10);
     assert_eq!(play(&[1, 2, 3], 2020), 27);
@@ -11,6 +12,15 @@ fn main() {
     assert_eq!(play(&[3, 1, 2], 2020), 1836);
 
     println!("(part1) The 2020th number is {}", play(&input[..], 2020));
+    println!("(part2) The 2020th number is {}", play(&input[..], 30000000));
+
+    assert_eq!(play(&[0, 3, 6], 30000000), 175594);
+    assert_eq!(play(&[1, 3, 2], 30000000), 2578);
+    assert_eq!(play(&[2, 1, 3], 30000000), 3544142);
+    assert_eq!(play(&[1, 2, 3], 30000000), 261214);
+    assert_eq!(play(&[2, 3, 1], 30000000), 6895259);
+    assert_eq!(play(&[3, 2, 1], 30000000), 18);
+    assert_eq!(play(&[3, 1, 2], 30000000), 362);
 }
 
 fn play(input: &[usize], num_turns: usize) -> usize {
@@ -25,6 +35,10 @@ fn play(input: &[usize], num_turns: usize) -> usize {
     let mut last_num: usize = input[input.len() - 1];
 
     for turn in input.len()..num_turns {
+        if (turn+1) % 1000000 == 0
+        {
+            println!("{}/{}", turn+1, num_turns);
+        }
         let current_num = match track.get(&last_num) {
             None => 0,
             Some(vd) => match vd.len() {
@@ -51,4 +65,55 @@ fn play(input: &[usize], num_turns: usize) -> usize {
         last_num = current_num;
     }
     last_num
+}
+
+fn advance(track: &mut BTreeMap<usize, usize>) {
+    for v in track.values_mut() {
+        *v += 1;
+    }
+}
+
+fn play_v2(input: &[usize], num_turns: usize) -> usize {
+    let mut track: BTreeMap<usize, usize> = BTreeMap::new();
+
+    for num in input.iter() {
+        advance(&mut track);
+        track.insert(*num, 0);
+    }
+    let mut seen_at: HashMap<BTreeMap<usize, usize>, usize> = HashMap::new();
+
+    let mut last: usize = input[input.len() - 1];
+    let mut turn = input.len();
+
+    let mut cycle_found = false;
+
+    while turn < num_turns {
+        let num = track.get(&last).cloned().unwrap_or(0);
+        // println!("[{}/{}] {} -> {}", turn, num_turns, last, num);
+        // println!("Pre-advance: {:#?}", track);
+        advance(&mut track);
+        track.insert(last, 1);
+        // println!("Post-advance: {:#?}", track);
+        last = num;
+
+        if !cycle_found {
+            match seen_at.get(&track) {
+                None => {
+                    seen_at.insert(track.clone(), turn);
+                }
+                Some(prev_turn) => {
+                    // we have found a previously visited state -> shorten
+                    let cycle_len = turn - prev_turn;
+
+                    let remaining = num_turns - turn;
+
+                    turn += (remaining / cycle_len) * cycle_len;
+                    cycle_found = true;
+                }
+            }
+        }
+        turn += 1;
+    }
+    println!("{}", last);
+    last
 }
